995e88246647dabb6ec335c2685362da1b397ca5
Brittany Dunn <dunnbrit@oregonstate.edu>
2019-05-05 22:53:20 -0700

    Ready to add writing to files

--- dunnbrit.buildrooms.c
+++ dunnbrit.buildrooms.c
@@ -21,7 +21,7 @@ struct Room{
 };
 
 /*Array for room types*/
-char* Type[3] = {"START_ROOM", "MID_ROOM", "END_ROOM"};
+char* RoomType[3] = {"START_ROOM", "MID_ROOM", "END_ROOM"};
 /*Array for file names*/
 char* FileName[10] = {"/blue_room", "/red_room", "/yellow_room", "/green_room", "/pink_room", "/purple_room", "/orange_room", "/black_room", "/white_room", "/silver_room"};
 /*Array for room names*/
@@ -166,6 +166,7 @@ int main(){
 /*Initialize Random*/
 srand(time(0));
 
+
 /*Create a new directory*/    
     /*Path for files in the directory*/
     char dirPath[] = "dunnbrit.buildrooms.";
@@ -184,8 +185,8 @@ srand(time(0));
 	exit;
     }
  
-/*Add 7 files to the new directory*/
-    /*Get 3 fileNames not to use*/
+
+/*Get 3 fileNames/Rooms not to use*/
     int skipNum[3];
     int i;    
     /*Copy dirName into new variable because it won't work after loop otherwise*/
@@ -201,15 +202,74 @@ srand(time(0));
 	}
     /*Make sure none of the numbers are the same*/
     }while(skipNum[0] == skipNum[1] || skipNum[1] == skipNum[2] || skipNum[0] == skipNum[2]);
+ 
     
+/*Create all the rooms before the files*/
     /*Call allocate to allocate memory for all the room structs*/
     struct Room* allRooms = 0;
     allRooms = allocate();
     
-    /*Make the new files and add room to array of rooms*/
+    /*Add selected rooms to allRooms*/
+    int index = 0;
+    for(i=0; i < 10; i++){
+	/*Check if the fileName is one to skip*/
+	if(i != skipNum[0] && i != skipNum[1] && i != skipNum[2]){
+	    /*If not then assign room in struct*/
+		/*Assign name to room in allRooms*/
+		allRooms[index].name = calloc(16,sizeof(char));
+		strcpy(allRooms[index].name, RoomName[i]);
+		/*Set number of connections to 0*/
+		allRooms[index].numOutboundConnections = 0;
+		/*Increment index*/
+		index++;
+	}
+    }
+    
+  
+/*Create all room connections in graph*/
+    /*Call IsGraphFull until enough connections have been made*/
+    while (IsGraphFull(allRooms) == 0){
+	/*Call AddRandomConnection to connect two rooms*/
+	AddRandomConnection(allRooms);
+    }
+
+/*Assign Room Types*/
+    int start;
+    int end;
+    
+    do{
+	/*Get random start room and end room*/
+	start = rand() % 7;
+	end = rand() % 7;
+    /*Repeat until two different rooms are selected*/
+    }while(start == end);
+    
+    /*Assign the room types*/
+    for(i=0;i < 7; i++){
+	/*Allocate memory*/
+	allRooms[i].roomType = calloc(16,sizeof(char));
+	/*if start room*/
+	if(i == start){
+	    /*Assign type START_ROOM*/
+	    strcpy(allRooms[i].roomType, RoomType[0]);
+	}
+	/*if end room*/
+	else if(i == end){
+	    /*Assign type END_ROOM*/
+	    strcpy(allRooms[i].roomType, RoomType[2]);	    
+	}
+	/*otherwise its a mid room*/
+	else{
+	    /*Assign type MID_ROOM*/
+	    strcpy(allRooms[i].roomType, RoomType[1]);
+	}
+    }
+	
+  
+/*Make the new files with room info*/
     int file_descriptor; /*used to check for errors*/
     char filepath[45];
-    int index = 0;
+    index = 0;
     for(i=0; i < 10; i++){
 	/*Check if the fileName is one to skip*/
 	if(i != skipNum[0] && i != skipNum[1] && i != skipNum[2]){
@@ -227,26 +287,24 @@ srand(time(0));
 	    if(file_descriptor < 0){
 		printf("Error with file creation");
 	    }
-	    /*If not then assign room in struct*/
+	    /*If not then add room info*/
 	    else{
-		/*Assign name to room in allRooms*/
-		allRooms[index].name = calloc(16,sizeof(char));
-		strcpy(allRooms[index].name, RoomName[i]);
-		/*Set number of connections to 0*/
-		allRooms[index].numOutboundConnections = 0;
+		printf("ROOM NAME: %s\n", allRooms[index].name);
+		/*Loop through all the connecting rooms*/
+		int j;
+		for(j=0; j < allRooms[index].numOutboundConnections; j++){
+		    printf("CONNECTION %d: %s\n", j, allRooms[index].outboundConnections[j]->name);
+		}
+		printf("ROOM TYPE: %s\n", allRooms[index].roomType);
 		/*Increment index*/
 		index++;
+	    
+		/*Close file*/
+		close(file_descriptor);
 	    }
 	}
     }
     
-/*Create all connections in graph*/
-    /*Call IsGraphFull until enough connections have been made*/
-    while (IsGraphFull(allRooms) == 0){
-	/*Call AddRandomConnection to connect two rooms*/
-	AddRandomConnection(allRooms);
-    }
-  
     
 return 0;
 }
